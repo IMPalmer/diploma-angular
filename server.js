9528c87b205b508d650212f2b637f2cf45a92ed0
IMPalmer <i.m.great.palmer@gmail.com>
2021-04-05 20:18:08 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express();
 const path = require('path');
-const port = process.env.PORT || 8080;
+
+const app = express();
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/diploma-angular'));
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/diploma-angular/index.html'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/diploma-angular/index.html'));
 });
-app.listen(port, () =>
-  console.log(`Server is running on: http://localhost:${port}`));
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
