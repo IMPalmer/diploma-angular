239b56301ec9e0f6ef287ccef469021fb424581c
IMPalmer <i.m.great.palmer@gmail.com>
2021-05-04 17:58:27 +0300

    updates

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -14,7 +14,7 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (!this.auth.user) {
-      this.router.navigate(['login']);
+      this.router.navigate(['sign-in']);
       return false;
     }
     return true;
